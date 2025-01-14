2308e41044c71a283598858136f4045bcbc6ad85
Kyamel <129009324+Kyamel@users.noreply.github.com>
2025-01-14 13:44:21 -0300

    update build

--- build.cmd
+++ build.cmd
@@ -1,12 +1,26 @@
 @echo off
 
-:: Activate virtual environment
-call .venv\Scripts\activate
+:: Verify flutter install
+flutter --version >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Flutter não encontrado, instale o Flutter
+    exit /b 1
+)
+
+:: Verify python virtual enviroment
+if exist ".venv" (
+    echo Ativando o ambiente virtual...
+    call .venv\Scripts\activate.bat
+) else (
+    echo Ambiente virtual não encontrado, criando ambiente...
+    python -m venv .venv
+    call .venv\Scripts\activate.bat
+)
 
 :: Execute python script
 python scripts\build.py windows
 
-:: Desativa a virtual environment
+:: Desctivates virtual enviroment
 deactivate
 
 :: Navigate to app directory
